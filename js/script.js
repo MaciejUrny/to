005efadabbbd74a6a9c08612f37c4da0d09e038c
Maciej Urny <maciej.urny@o2.pl>
2022-12-28 15:44:27 +0100

    adding new tasks

---
+++ js/script.js
@@ -0,0 +1,56 @@
+{
+    const tasks = [
+        {
+            content: "nagrać lekcję",
+            done: false,
+        },
+        {
+            content: "zjeść pierogi",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => { 
+        tasks.push({
+            content: newTaskContent,
+        })
+        
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if (newTaskContent === ""){
+            return;
+        };
+
+        addNewTask(newTaskContent);
+     };
+
+    const init = () => {
+        render();
+
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
