2b4cf8c545b2b94c5adc73185a9c85e1a8c8397b
Maciej Urny <maciej.urny@o2.pl>
2023-01-01 23:26:13 +0100

    js modified

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "zjeść pierogi",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const refreshInput = () => {
         const newTaskInput = document.querySelector(".js-newTask");
